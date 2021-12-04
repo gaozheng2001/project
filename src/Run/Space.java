3c854f2c5d03389450c3f0172f4f69f32be66c0c
zheng gao <1152046539@qq.com>
2021-12-04 22:52:22 +0800

    adding boardcheck

--- src/Run/Space.java
+++ src/Run/Space.java
@@ -6,11 +6,12 @@ import Thing.Force;
 public class Space {
     public Ball[] balls;
     public Force[][] forces;
+    public int boardlength;
 
-    public Space(Ball[] balls, Force[][] forces, int BoaldLength){
+    public Space(Ball[] balls, Force[][] forces, int BoardLength){
         this.balls = balls;
         this.forces = forces;
-
+        this.boardlength = BoardLength;
     }
 
     public double getDistance(Ball ball1 , Ball ball2){
@@ -21,5 +22,20 @@ public class Space {
         double distance = getDistance(ball1, ball2);
         double r = 0.1; //r为允许误差，用于后期调试
         return (distance <= ball1.getBALL_R() + ball2.getBall_R() + r);
+    }
+
+    public boolean checkboardx(Ball ball){
+        return ball.getBallRX() <= ball.getBALL_R() || ball.getBallRX() >= boardlength - ball.getBall_R();
+    }
+
+    public boolean checkboardy(Ball ball){
+        return ball.getBallRY() <= ball.getBALL_R() || ball.getBallRY() >= boardlength - ball.getBall_R();
+    }
+
+    public void boardcheck(Ball ball){
+        if (checkboardx(ball))
+            ball.setVelocityX(-ball.getVelocityX());
+        if (checkboardy(ball))
+            ball.setVelocityY(-ball.getVelocityY());
     }
 }
